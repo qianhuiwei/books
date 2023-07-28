21b8f58a10312888ff48f8fa421799d2042a2b76
qianhuiwei <u1041216@utan.edu>
2023-07-28 18:13:17 +0800

    phase 2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from './components/BookCreate';
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const createBook = (title) => {
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+
+        console.log(response);
+
         const updateBooks = [
             ...books,
-            { 
-                id: Math.round(Math.random() * 9999),
-                title 
-            }
+            response.data
         ];
+
         setBooks(updateBooks);
     };
 
-    const deleteBookById = (id) => {
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
         });
-        setBooks(updatedBooks);
-    };
 
-    const editBookById = (id, newTitle) => {
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return {...book, title: newTitle};
+                return {...book, ...response.data};
             }
             return book;
         });
+
+        setBooks(updatedBooks);
+    };
+
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+
         setBooks(updatedBooks);
     };
 
